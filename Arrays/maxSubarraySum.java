d6424b0b93762dbcd387bbeb63f4248fa5531e22
Disha Rathore <disharathore126@gmail.com>
2025-02-06 16:01:55 +0530

    New commit

--- Arrays/maxSubarraySum.java
+++ Arrays/maxSubarraySum.java
@@ -1,4 +1,6 @@
 public class maxSubarraySum{
+
+    //Brute Force Approach
     public static int subArr(int arr[]){
         int largest = Integer.MIN_VALUE;
         for(int i=0 ; i<arr.length ; i++){
@@ -22,8 +24,41 @@ public class maxSubarraySum{
         return largest;
     }
 
+    //PREFIX Sum Approach
+    public static int subArr2(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0 ; i<arr.length ; i++){
+            int sum = 0;
+            for(int j=i ; j<arr.length ; j++){
+                sum = sum+arr[j];
+                System.out.print ("sum:" +sum);
+            }
+            if(sum > largest){
+                largest = sum;
+            }
+
+            System.out.println();
+        }
+        return largest;
+    }
+
+    //Kadans Approach
+    public static int subArr3(int arr[]){
+        int currSum = 0 ;
+        int maxSum = Integer.MIN_VALUE;
+        
+        for(int i=0 ; i<arr.length ; i++){
+            currSum += arr[i];
+            if(currSum < 0){
+                currSum = 0;
+            }
+            maxSum = Math.max(currSum,maxSum);
+        }
+        return maxSum;
+    }
+
     public static void main(String args[]){
-        int arr[] = {2,4,6,8,10};
-        System.out.print("Largest sum is : " +subArr(arr));
+        int arr[] = {-2,-3,4,-1,-2,1,5,-3};
+        System.out.print("Max sum is : " +subArr3(arr));
     }
 }
